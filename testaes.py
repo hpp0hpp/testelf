29db49f617dbe7babdb7b95a4a1e0fe3a035bd61
hpp <hpp0hpp@hotmail.com>
2025-11-10 21:22:44 +0800

    5

---
+++ testaes.py
@@ -0,0 +1,9 @@
+from aes import AES
+text = "cBDw1t5m3WC9vH+9v7zBcHYHc75D1e0mbXuod2yPcqDZ1tImcWCpvtiTv2st+HeZbtzCvNyN32yDc8+937sh+85Cb8cw+/CMcH6NvHeHcI=="
+key = "ziISjqkXPsGUMRNGyWigxDGtJbfTdcGv"
+iv = "WonrnVkxeIxDcFbv"
+
+
+aes=AES(key.encode('utf-8'))
+encrypted = aes.encrypt_cbc(text.encode('utf-8'), iv.encode('utf-8'))
+print(encrypted.hex())
